1f5867e5f13b5af6d6c5096df4ca7f412b7baf14
Newbot <58357514+NewBotMD@users.noreply.github.com>
2019-12-01 12:18:59 +0300

    Update locks.py

--- handlers/locks.py
+++ handlers/locks.py
@@ -1,10 +1,10 @@
 from utlis.rank import setrank,isrank,remrank,remsudos,setsudo
 from utlis.send import send_msg, Name
+from utlis.tg import Bot
+import importlib
 
 import threading, requests, time, random, re
 from config import *
-import importlib
-from utlis.tg import Bot
 
 
 def locks(client, message,redis):
@@ -32,14 +32,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Llink",T,redis)
     else:
       save = redis.sadd("{}Nbot:Llink".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Llink",T,redis)
 
   if text == c.Uurl :
     get = redis.sismember("{}Nbot:Llink".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Llink".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Llink",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Llink",T,redis)
@@ -50,14 +48,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lphoto",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lphoto".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lphoto",T,redis)
 
   if text == c.Uphoto :
     get = redis.sismember("{}Nbot:Lphoto".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lphoto".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lphoto",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lphoto",T,redis)
@@ -68,14 +64,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lusername",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lusername".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lusername",T,redis)
 
   if text == c.Uusername :
     get = redis.sismember("{}Nbot:Lusername".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lusername".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lusername",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lusername",T,redis)
@@ -86,14 +80,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Ltag",T,redis)
     else:
       save = redis.sadd("{}Nbot:Ltag".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Ltag",T,redis)
 
   if text == c.Utag :
     get = redis.sismember("{}Nbot:Ltag".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Ltag".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Ltag",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Ltag",T,redis)
@@ -104,14 +96,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lvideo",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lvideo".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lvideo",T,redis)
 
   if text == c.Uvideo :
     get = redis.sismember("{}Nbot:Lvideo".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lvideo".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lvideo",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lvideo",T,redis)
@@ -122,14 +112,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lgifs",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lgifs".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lgifs",T,redis)
 
   if text == c.Ugifs :
     get = redis.sismember("{}Nbot:Lgifs".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lgifs".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lgifs",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lgifs",T,redis)
@@ -140,14 +128,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lsticker",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lsticker".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lsticker",T,redis)
 
   if text == c.Usticker :
     get = redis.sismember("{}Nbot:Lsticker".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lsticker".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lsticker",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lsticker",T,redis)
@@ -158,14 +144,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lfiles",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lfiles".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lfiles",T,redis)
 
   if text == c.Ufiles :
     get = redis.sismember("{}Nbot:Lfiles".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lfiles".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lfiles",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lfiles",T,redis)
@@ -176,14 +160,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lmusic",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lmusic".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lmusic",T,redis)
 
   if text == c.Umusic :
     get = redis.sismember("{}Nbot:Lmusic".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lmusic".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lmusic",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lmusic",T,redis)
@@ -194,14 +176,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lfwd",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lfwd".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lfwd",T,redis)
 
   if text == c.Ufwd :
     get = redis.sismember("{}Nbot:Lfwd".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lfwd".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lfwd",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lfwd",T,redis)
@@ -212,14 +192,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lvoice",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lvoice".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lvoice",T,redis)
 
   if text == c.Uvoice :
     get = redis.sismember("{}Nbot:Lvoice".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lvoice".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lvoice",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lvoice",T,redis)
@@ -230,14 +208,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lcontact",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lcontact".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lcontact",T,redis)
 
   if text == c.Ucontact :
     get = redis.sismember("{}Nbot:Lcontact".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lcontact".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lcontact",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lcontact",T,redis)
@@ -248,14 +224,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lmarkdown",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lmarkdown".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lmarkdown",T,redis)
 
   if text == c.Umarkdown :
     get = redis.sismember("{}Nbot:Lmarkdown".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lmarkdown".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lmarkdown",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lmarkdown",T,redis)
@@ -266,14 +240,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lbots",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lbots".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lbots",T,redis)
 
   if text == c.Ubots :
     get = redis.sismember("{}Nbot:Lbots".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lbots".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lbots",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lbots",T,redis)
@@ -284,14 +256,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Ledits",T,redis)
     else:
       save = redis.sadd("{}Nbot:Ledits".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Ledits",T,redis)
 
   if text == c.Uedits :
     get = redis.sismember("{}Nbot:Ledits".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Ledits".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Ledits",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Ledits",T,redis)
@@ -302,14 +272,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Larabic",T,redis)
     else:
       save = redis.sadd("{}Nbot:Larabic".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Larabic",T,redis)
 
   if text == c.Uarabic :
     get = redis.sismember("{}Nbot:Larabic".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Larabic".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Larabic",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Larabic",T,redis)
@@ -320,14 +288,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lenglish",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lenglish".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lenglish",T,redis)
 
   if text == c.Uenglish :
     get = redis.sismember("{}Nbot:Lenglish".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lenglish".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lenglish",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lenglish",T,redis)
@@ -340,14 +306,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Linline",T,redis)
     else:
       save = redis.sadd("{}Nbot:Linline".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Linline",T,redis)
 
   if text == c.Uinline :
     get = redis.sismember("{}Nbot:Linline".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Linline".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Linline",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Linline",T,redis)
@@ -358,14 +322,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lchat",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lchat".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lchat",T,redis)
 
   if text == c.Uchat :
     get = redis.sismember("{}Nbot:Lchat".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lchat".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lchat",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lchat",T,redis)
@@ -376,14 +338,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Ljoin",T,redis)
     else:
       save = redis.sadd("{}Nbot:Ljoin".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Ljoin",T,redis)
 
   if text == c.Ujoin :
     get = redis.sismember("{}Nbot:Ljoin".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Ljoin".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Ljoin",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Ljoin",T,redis)
@@ -396,14 +356,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Llongtext",T,redis)
     else:
       save = redis.sadd("{}Nbot:Llongtext".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Llongtext",T,redis)
 
   if text == c.Ulongtext :
     get = redis.sismember("{}Nbot:Llongtext".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Llongtext".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Llongtext",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Llongtext",T,redis)
@@ -414,14 +372,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lall",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lall".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lall",T,redis)
 
   if text == c.Uall :
     get = redis.sismember("{}Nbot:Lall".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lall".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lall",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lall",T,redis)
@@ -432,14 +388,12 @@ def locks(client, message,redis):
       send_msg("LUN",client, message,r.locked,"Lnote",T,redis)
     else:
       save = redis.sadd("{}Nbot:Lnote".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.lock,"Lnote",T,redis)
 
   if text == c.Unote :
     get = redis.sismember("{}Nbot:Lnote".format(BOT_ID),chatID)
     if get :
       save = redis.srem("{}Nbot:Lnote".format(BOT_ID),chatID)
-      
       send_msg("LU",client, message,r.unlock,"Lnote",T,redis)
     else:
       send_msg("LUN",client, message,r.unlocked,"Lnote",T,redis)
@@ -451,7 +405,6 @@ def locks(client, message,redis):
         if get :
           save = redis.srem("{}Nbot:Lpin".format(BOT_ID),chatID)
           Bot("sendMessage",{"chat_id":chatID,"text":r.ADD.format(BY,R),"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
-
         else:
           Bot("sendMessage",{"chat_id":chatID,"text":r.ADDed.format(BY,R),"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
 
@@ -485,8 +438,6 @@ def locks(client, message,redis):
       else:
         save = redis.sadd("{}Nbot:welcomeSend".format(BOT_ID),chatID)
         Bot("sendMessage",{"chat_id":chatID,"text":r.unADD.format(BY,R),"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
-
-
 
     if text == c.Lreply :
       R = text.split(" ")[1]
@@ -527,9 +478,6 @@ def locks(client, message,redis):
       else:
         save = redis.sadd("{}Nbot:ReplySendBOT".format(BOT_ID),chatID)
         Bot("sendMessage",{"chat_id":chatID,"text":r.unADD.format(BY,R),"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
-
-
-
 
     if text == c.Lkickme :
       R = text.split(" ")[1]
