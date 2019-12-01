2b6db0e2c9bae8ca612996c701e303e4f862350e
Newbot <58357514+NewBotMD@users.noreply.github.com>
2019-12-01 14:15:52 +0300

    Update and rename config.py to setup.py

--- config.py
+++
@@ -1,5 +0,0 @@
-TOKEN = ""
-BOT_ID = TOKEN.split(":")[0]
-SUDO = 
-API_ID = 
-API_HASH = ""
---
+++ setup.py
@@ -0,0 +1,31 @@
+import requests,os
+
+out ="""
+API_ID = 770767
+API_HASH = '5ce182752b91a16f90e88f96a541f331'
+"""
+def Bot(TOKEN,method,data):
+  url = "https://api.telegram.org/bot{}/{}".format(TOKEN,method)
+  post = requests.post(url,data=data)
+  #print(post.json())
+  return post.json()
+ID = ""
+go = True
+while go:
+  token = input("input you're bot TOKEN:")
+  get = Bot(token,"getme",{})
+  if get["ok"]:
+    out = out+"\n"+"TOKEN = '{}'\nBOT_ID = int(TOKEN.split(':')[0])".format(token)
+    go = False
+    ID = int(token.split(':')[0])
+  else:
+    print("TOKEN is invalid, Try again")
+
+sudo = input("input you're ID:")
+out = out+"\n"+"SUDO = int({})".format(sudo)
+
+f = open("config.py","w+") 
+f.write(out)
+f.close()
+
+os.system('cd ~;cdNewBot;pm2 start bot.py --name {} --interpreter python3.7 --interpreter-args -u'.format(ID))
