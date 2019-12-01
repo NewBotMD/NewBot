c0bee49d6f5bf67862cf9f3659709479fc01f99a
Newbot <58357514+NewBotMD@users.noreply.github.com>
2019-12-01 12:07:57 +0300

    Add files via upload

---
+++ utlis/locks.py
@@ -0,0 +1,310 @@
+from utlis.rank import setrank,isrank,remrank,remsudos,setsudo
+import threading, requests, time, random, re, json
+from config import *
+import importlib
+
+from os import listdir
+from os.path import isfile, join
+
+from pyrogram import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+def getOR(rank,r,userID):
+  if rank == "admin":
+    reply_markup=InlineKeyboardMarkup([
+
+[InlineKeyboardButton(r.lockC,callback_data=json.dumps(["ShowOlock","",userID])),InlineKeyboardButton(r.AdminC,callback_data=json.dumps(["ShowOadmin","",userID])),],
+
+
+])
+
+  if rank == "owner":
+    reply_markup=InlineKeyboardMarkup([
+
+[InlineKeyboardButton(r.lockC,callback_data=json.dumps(["ShowOlock","",userID])),InlineKeyboardButton(r.AdminC,callback_data=json.dumps(["ShowOadmin","",userID])),],
+[InlineKeyboardButton(r.OwnerC,callback_data=json.dumps(["ShowOowner","",userID])),],
+
+])
+
+  if rank == "creator":
+    reply_markup=InlineKeyboardMarkup([
+
+[InlineKeyboardButton(r.lockC,callback_data=json.dumps(["ShowOlock","",userID])),InlineKeyboardButton(r.AdminC,callback_data=json.dumps(["ShowOadmin","",userID])),],
+[InlineKeyboardButton(r.OwnerC,callback_data=json.dumps(["ShowOowner","",userID])),InlineKeyboardButton(r.CreatorC,callback_data=json.dumps(["ShowOcreator","",userID])),],
+
+])
+
+  if rank == "sudos":
+    reply_markup=InlineKeyboardMarkup([
+
+[InlineKeyboardButton(r.lockC,callback_data=json.dumps(["ShowOlock","",userID])),InlineKeyboardButton(r.AdminC,callback_data=json.dumps(["ShowOadmin","",userID])),],
+
+[InlineKeyboardButton(r.OwnerC,callback_data=json.dumps(["ShowOowner","",userID])),InlineKeyboardButton(r.CreatorC,callback_data=json.dumps(["ShowOcreator","",userID])),],
+
+[InlineKeyboardButton(r.SudosC,callback_data=json.dumps(["ShowOsudos","",userID])),],
+
+])
+
+  if rank == "sudo":
+    reply_markup=InlineKeyboardMarkup([
+
+[InlineKeyboardButton(r.lockC,callback_data=json.dumps(["ShowOlock","",userID])),InlineKeyboardButton(r.AdminC,callback_data=json.dumps(["ShowOadmin","",userID])),],
+
+[InlineKeyboardButton(r.OwnerC,callback_data=json.dumps(["ShowOowner","",userID])),InlineKeyboardButton(r.CreatorC,callback_data=json.dumps(["ShowOcreator","",userID])),],
+
+[InlineKeyboardButton(r.SudosC,callback_data=json.dumps(["ShowOsudos","",userID])),InlineKeyboardButton(r.SudoC,callback_data=json.dumps(["ShowOsudo","",userID])),],
+
+
+])
+  return (reply_markup or "")
+def st(client, message,redis,type = 1):
+  userID = message.from_user.id
+  if (hasattr(message,"chat")):
+    chatID = message.chat.id
+  else:
+    chatID = message.message.chat.id
+  #print("vvvvvvvvvvvvvvv",chatID)
+  rank = isrank(redis,userID,chatID)
+  
+  if redis.sismember("{}Nbot:lang:ar".format(BOT_ID),chatID):
+    lang = "ar"
+  elif redis.sismember("{}Nbot:lang:en".format(BOT_ID),chatID):
+    lang = "en"
+  else :
+    lang = "ar"
+  moduleCMD = "lang."+lang+"-cmd"
+  moduleREPLY = "lang."+lang+"-reply"
+  c = importlib.import_module(moduleCMD)
+  r = importlib.import_module(moduleREPLY)
+  if redis.sismember("{}Nbot:Llink".format(BOT_ID),chatID):
+    link = c.STlink+" "+r.false
+  else:
+    link = c.STlink+" "+r.true
+
+  if redis.sismember("{}Nbot:Lusername".format(BOT_ID),chatID):#2
+    username = c.STusername+" "+r.false
+  else:
+    username = c.STusername+" "+r.true
+
+  if redis.sismember("{}Nbot:Ltag".format(BOT_ID),chatID):#3
+    tag = c.STtag+" "+r.false
+  else:
+    tag = c.STtag+" "+r.true
+
+  if redis.sismember("{}Nbot:Lenglish".format(BOT_ID),chatID):#4
+    english = c.STenglish+" "+r.false
+  else:
+    english = c.STenglish+" "+r.true
+
+  if redis.sismember("{}Nbot:Larabic".format(BOT_ID),chatID):#5
+    arabic = c.STarabic+" "+r.false
+  else:
+    arabic = c.STarabic+" "+r.true
+
+  if redis.sismember("{}Nbot:Lmarkdown".format(BOT_ID),chatID):#6
+    markdown = c.STmarkdown+" "+r.false
+  else:
+    markdown = c.STmarkdown+" "+r.true
+
+  if redis.sismember("{}Nbot:Linline".format(BOT_ID),chatID):#7
+    inline = c.STinline+" "+r.false
+  else:
+    inline = c.STinline+" "+r.true
+
+  if redis.sismember("{}Nbot:Lsticker".format(BOT_ID),chatID):#8
+    sticker = c.STsticker+" "+r.false
+  else:
+    sticker = c.STsticker+" "+r.true
+
+  if redis.sismember("{}Nbot:Lgifs".format(BOT_ID),chatID):#9
+    gifs = c.STgifs+" "+r.false
+  else:
+    gifs = c.STgifs+" "+r.true
+
+  if redis.sismember("{}Nbot:Lvideo".format(BOT_ID),chatID):#10
+    video = c.STvideo+" "+r.false
+  else:
+    video = c.STvideo+" "+r.true
+
+  if redis.sismember("{}Nbot:Lvoice".format(BOT_ID),chatID):#11
+    voice = c.STvoice+" "+r.false
+  else:
+    voice = c.STvoice+" "+r.true
+
+  if redis.sismember("{}Nbot:Lmusic".format(BOT_ID),chatID):#12
+    music = c.STmusic+" "+r.false
+  else:
+    music = c.STmusic+" "+r.true
+
+  if redis.sismember("{}Nbot:Lfiles".format(BOT_ID),chatID):#13
+    files = c.STfiles+" "+r.false
+  else:
+    files = c.STfiles+" "+r.true
+
+  if redis.sismember("{}Nbot:Lphoto".format(BOT_ID),chatID):#14
+    photo = c.STphoto+" "+r.false
+  else:
+    photo = c.STphoto+" "+r.true
+
+  if redis.sismember("{}Nbot:Lcontact".format(BOT_ID),chatID):#15
+    contact = c.STcontact+" "+r.false
+  else:
+    contact = c.STcontact+" "+r.true
+
+  if redis.sismember("{}Nbot:Lbots".format(BOT_ID),chatID):#16
+    bots = c.STbots+" "+r.false
+  else:
+    bots = c.STbots+" "+r.true
+
+  if redis.sismember("{}Nbot:Ljoin".format(BOT_ID),chatID):#17
+    join = c.STjoin+" "+r.false
+  else:
+    join = c.STjoin+" "+r.true
+
+  if redis.sismember("{}Nbot:Lfwd".format(BOT_ID),chatID):#18
+    fwd = c.STfwd+" "+r.false
+  else:
+    fwd = c.STfwd+" "+r.true
+
+  if redis.sismember("{}Nbot:Lnote".format(BOT_ID),chatID):#19
+    note = c.STnote+" "+r.false
+  else:
+    note = c.STnote+" "+r.true
+  if redis.sismember("{}Nbot:Ledits".format(BOT_ID),chatID):#19
+    edits = c.STedits+" "+r.false
+  else:
+    edits = c.STedits+" "+r.true
+  if redis.sismember("{}Nbot:Llongtext".format(BOT_ID),chatID):#19
+    longtext = c.STlongtext+" "+r.false
+  else:
+    longtext = c.STlongtext+" "+r.true
+
+  if redis.sismember("{}Nbot:Lflood".format(BOT_ID),chatID):#19
+    flood = c.STflood+" "+r.false
+  else:
+    flood = c.STflood+" "+r.true
+  if not redis.sismember("{}Nbot:welcomeSend".format(BOT_ID),chatID):
+    welcomeSend = c.STwelcomeSend+" "+r.true
+  else:
+    welcomeSend = c.STwelcomeSend+" "+r.false
+
+  if not redis.sismember("{}Nbot:ReplySend".format(BOT_ID),chatID):
+    ReplySend = c.STReplySend+" "+r.true
+  else:
+    ReplySend = c.STReplySend+" "+r.false
+
+  if not redis.sismember("{}Nbot:ReplySendBOT".format(BOT_ID),chatID):
+    ReplySendBOT = c.STReplySendBOT+" "+r.true
+  else:
+    ReplySendBOT = c.STReplySendBOT+" "+r.false
+
+  if not redis.sismember("{}Nbot:kickme".format(BOT_ID),chatID):
+    kickme = c.STkickme+" "+r.true
+  else:
+    kickme = c.STkickme+" "+r.false
+
+  if redis.sismember("{}Nbot:Lpin".format(BOT_ID),chatID):
+    pin = c.STpin+" "+r.false
+  else:
+    pin = c.STpin+" "+r.true
+
+  if not redis.sismember("{}Nbot:IDSend".format(BOT_ID),chatID):
+    IDSend = c.STIDSend+" "+r.true
+  else:
+    IDSend = c.STIDSend+" "+r.false
+
+  if type == 1:
+    reply_markup=InlineKeyboardMarkup([
+        [
+          InlineKeyboardButton(link,callback_data=json.dumps(["LU","Llink",userID,1])),
+          InlineKeyboardButton(bots,callback_data=json.dumps(["LU","Lbots",userID,1])),
+          ],[
+            InlineKeyboardButton(video,callback_data=json.dumps(["LU","Lvideo",userID,1])),
+            InlineKeyboardButton(note,callback_data=json.dumps(["LU","Lnote",userID,1])),
+            ],[
+              InlineKeyboardButton(music,callback_data=json.dumps(["LU","Lmusic",userID,1])),
+              InlineKeyboardButton(voice,callback_data=json.dumps(["LU","Lvoice",userID,1]))
+              ],[
+                InlineKeyboardButton(gifs,callback_data=json.dumps(["LU","Lgifs",userID,1])),
+                InlineKeyboardButton(photo,callback_data=json.dumps(["LU","Lphoto",userID,1])),
+                ],[
+                  InlineKeyboardButton(">>",callback_data=json.dumps(["listCH",2,userID])),
+                  ]
+                  ])
+
+  if type == 2:
+    reply_markup=InlineKeyboardMarkup([
+        [
+          InlineKeyboardButton(inline,callback_data=json.dumps(["LU","Linline",userID,2])),
+          InlineKeyboardButton(markdown,callback_data=json.dumps(["LU","Lmarkdown",userID,2])),
+          ],[
+            InlineKeyboardButton(files,callback_data=json.dumps(["LU","Lfiles",userID,2])),
+            InlineKeyboardButton(contact,callback_data=json.dumps(["LU","Lcontact",userID,2])),
+            ],[
+              InlineKeyboardButton(sticker,callback_data=json.dumps(["LU","Lsticker",userID,2])),
+              InlineKeyboardButton(fwd,callback_data=json.dumps(["LU","Lfwd",userID,2])),
+              ],[
+                InlineKeyboardButton(username,callback_data=json.dumps(["LU","Lusername",userID,2])),
+                InlineKeyboardButton(english,callback_data=json.dumps(["LU","Lenglish",userID,2])),
+                ],[
+                  InlineKeyboardButton("<<",callback_data=json.dumps(["listCH",1,userID])),
+                  InlineKeyboardButton(">>",callback_data=json.dumps(["listCH",3,userID])),
+                  ]
+                  ])
+
+  if type == 3:
+    print(rank)
+    if rank != "admin":
+      t = [InlineKeyboardButton("<<",callback_data=json.dumps(["listCH",2,userID])),InlineKeyboardButton(">>",callback_data=json.dumps(["listCH",4,userID]))]
+    else:
+      t = [InlineKeyboardButton("<<",callback_data=json.dumps(["listCH",2,userID])),]
+    reply_markup=InlineKeyboardMarkup([
+        [
+                       InlineKeyboardButton(arabic,callback_data=json.dumps(["LU","Larabic",userID,3])),
+                    InlineKeyboardButton(tag,callback_data=json.dumps(["LU","Ltag",userID,3])),
+          ],[
+                    InlineKeyboardButton(join,callback_data=json.dumps(["LU","Ljoin",userID,3])),
+                    InlineKeyboardButton(edits,callback_data=json.dumps(["LU","Ledits",userID,3])),
+            ],[
+                    InlineKeyboardButton(longtext,callback_data=json.dumps(["LU","Llongtext",userID,3])),
+              ],t
+                  ])
+  if type == 4:
+    reply_markup=InlineKeyboardMarkup([
+        [
+                    InlineKeyboardButton(flood,callback_data=json.dumps(["LU","Lflood",userID,4])),
+                    InlineKeyboardButton(welcomeSend,callback_data=json.dumps(["LU","welcomeSend",userID,4])),
+          ],[
+                    InlineKeyboardButton(ReplySend,callback_data=json.dumps(["LU","ReplySend",userID,4])),
+                    InlineKeyboardButton(ReplySendBOT,callback_data=json.dumps(["LU","ReplySendBOT",userID,4])),
+            ],[
+                    InlineKeyboardButton(kickme,callback_data=json.dumps(["LU","kickme",userID,4])),
+                    InlineKeyboardButton(pin,callback_data=json.dumps(["LU","Lpin",userID,4])),
+              ],[
+                    InlineKeyboardButton(IDSend,callback_data=json.dumps(["LU","IDSend",userID,4])),
+              ],[
+                  InlineKeyboardButton("<<",callback_data=json.dumps(["listCH",3,userID])),
+                  ]
+                  ])
+  return reply_markup
+
+def Cklang(name,r,redis,chatID):
+  if redis.sismember("{}Nbot:lang:{}".format(BOT_ID,name),chatID):
+    return r.true
+  else:
+    return ""
+def Clang(client, message,redis,r):
+  if (hasattr(message,"chat")):
+    chatID = message.chat.id
+  else:
+    chatID = message.message.chat.id
+  userID = message.from_user.id
+  onlyfiles = [f for f in listdir("lang") if isfile(join("lang", f))]
+  array = []
+  names = []
+  i = 0
+  for f in onlyfiles:
+    name = f.split("-")[0]
+    if name not in names:
+      array.append([InlineKeyboardButton(r.lang[name]+" "+Cklang(name,r,redis,chatID),callback_data=json.dumps(["Chlang",name,userID]))])
+      names.append(name)
+      i +=1
+  return InlineKeyboardMarkup(array)
---
+++ utlis/send.py
@@ -0,0 +1,215 @@
+from utlis.rank import setrank,isrank,remrank,remsudos,setsudo
+from utlis.tg import Bot
+import threading, requests, time, random, re, json
+from config import *
+import importlib
+
+from pyrogram import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def send_msg(type,client, message,textM,Lhash,T,redis):
+  userID = message.from_user.id
+  userFN = message.from_user.first_name
+  chatID = message.chat.id
+  text = message.text
+  if redis.sismember("{}Nbot:lang:ar".format(BOT_ID),chatID):
+    lang = "ar"
+  elif redis.sismember("{}Nbot:lang:en".format(BOT_ID),chatID):
+    lang = "en"
+  else :
+    lang = "ar"
+  moduleCMD = "lang."+lang+"-cmd"
+  moduleREPLY = "lang."+lang+"-reply"
+  c = importlib.import_module(moduleCMD)
+  r = importlib.import_module(moduleREPLY)
+  if type == "LU":
+    if re.search(c.stL, text):
+      Tp = "LtoU"
+    if re.search(c.stU, text):
+      Tp = "UtoL"
+    BY = "<a href=\"tg://user?id={}\">{}</a>".format(userID,userFN)
+    tx = textM.format(BY,T)
+    b = json.dumps(["LandU",Lhash,userID,Tp])
+    v = InlineKeyboardMarkup([[InlineKeyboardButton(r.Corder, callback_data=b)]])
+    Bot("sendMessage",{"chat_id":chatID,"text":tx,"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True,"reply_markup":v})
+  if type == "LUN":
+    BY = "<a href=\"tg://user?id={}\">{}</a>".format(userID,userFN)
+    tx = textM.format(BY,T)
+    Bot("sendMessage",{"chat_id":chatID,"text":tx,"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
+
+  if type == "UD":
+    R = text.split(" ")[1]
+    userId = T.id
+    userFn = Name(T.first_name)
+    BY = "<a href=\"tg://user?id={}\">{}</a>".format(userId,userFn)
+    tx = textM.format(BY,R)
+    Bot("sendMessage",{"chat_id":chatID,"text":tx,"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
+
+  if type == "BN":
+    userId = T.id
+    userFn = Name(T.first_name)
+    BY = "[{}](tg://user?id={})".format(userFn,userId)
+    tx = textM.format(BY)
+    if re.search(c.stC, text):
+      Tp = "UtoB"
+    else:
+      Tp = "BtoU"
+    b = json.dumps(["Corder",Lhash,userID,userId,Tp])
+    v = InlineKeyboardMarkup([[InlineKeyboardButton(r.Corder, callback_data=b)]])
+    Bot("sendMessage",{"chat_id":chatID,"text":tx,"reply_to_message_id":message.message_id,"parse_mode":"markdown","disable_web_page_preview":True,"reply_markup":v})
+  
+  if type == "BNN":
+    userId = T.id
+    userFn = Name(T.first_name)
+    BY = "<a href=\"tg://user?id={}\">{}</a>".format(userId,userFn)
+    tx = textM.format(BY)
+    Bot("sendMessage",{"chat_id":chatID,"text":tx,"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
+  
+
+
+def BYusers(arrays,chatID,redis,client):
+  users = ""
+  i = 0
+  if redis.sismember("{}Nbot:lang:ar".format(BOT_ID),chatID):
+    lang = "ar"
+  elif redis.sismember("{}Nbot:lang:en".format(BOT_ID),chatID):
+    lang = "en"
+  else :
+    lang = "ar"
+  moduleCMD = "lang."+lang+"-cmd"
+  moduleREPLY = "lang."+lang+"-reply"
+  c = importlib.import_module(moduleCMD)
+  r = importlib.import_module(moduleREPLY)
+  for user in arrays:
+    i +=1
+    try:
+      getUser = client.get_users(user)
+      userId = getUser.id
+      userFn = getUser.first_name
+      users = users+"\n"+str(i)+" - "+"[{}](tg://user?id={})".format(userFn,userId)
+      print(userFn)
+    except Exception as e:
+      users = users+"\n"+str(i)+" - "+"[{}](tg://user?id={})".format(user,user)
+      print(e)
+      print(user)
+  return users
+
+def CKsend(redis,callback_query,type,ck,sID):
+  if ck["ok"] == False and not redis.sismember("{}Nbot:disabledgroups".format(BOT_ID),sID):
+    redis.sadd("{}Nbot:dontsend".format(BOT_ID),sID)
+    if type == "privates":
+      redis.srem("{}Nbot:privates".format(BOT_ID),sID)
+    else:
+      redis.srem("{}Nbot:groups".format(BOT_ID),sID)
+  elif ck["ok"]:
+    redis.sadd("{}Nbot:donesend".format(BOT_ID),sID)
+
+def Sendto(redis,callback_query,type):
+  IDS = redis.smembers("{}Nbot:{}".format(BOT_ID,type))
+  print(IDS)
+
+  if callback_query.message.reply_to_message.text:
+    for sID in IDS:
+     ck = Bot("sendMessage",{"chat_id":sID,"text":callback_query.message.reply_to_message.text,"parse_mode":"html"})
+     CKsend(redis,callback_query,type,ck,sID)
+     time.sleep(0.3)
+
+  if callback_query.message.reply_to_message.photo:
+    ID = callback_query.message.reply_to_message.photo.file_id
+    CP = callback_query.message.reply_to_message.caption
+    for sID in IDS:
+     ck = Bot("sendphoto",{"chat_id":sID,"photo":ID,"caption":CP,"parse_mode":"html"})
+     CKsend(redis,callback_query,type,ck,sID)
+     time.sleep(0.3)
+
+  if callback_query.message.reply_to_message.video:
+    ID = callback_query.message.reply_to_message.video.file_id
+    CP = callback_query.message.reply_to_message.caption
+    for sID in IDS:
+     ck = Bot("sendvideo",{"chat_id":sID,"video":ID,"caption":CP,"parse_mode":"html"})
+     CKsend(redis,callback_query,type,ck,sID)
+     time.sleep(0.3)
+
+  if callback_query.message.reply_to_message.video_note:
+    ID = callback_query.message.reply_to_message.video_note.file_id
+    CP = callback_query.message.reply_to_message.caption
+    for sID in IDS:
+     ck = Bot("sendVideoNote",{"chat_id":sID,"video_note":ID,"caption":CP,"parse_mode":"html"})
+     CKsend(redis,callback_query,type,ck,sID)
+     time.sleep(0.3)
+
+  if callback_query.message.reply_to_message.voice:
+    ID = callback_query.message.reply_to_message.voice.file_id
+    CP = callback_query.message.reply_to_message.caption
+    for sID in IDS:
+     ck = Bot("sendvoice",{"chat_id":sID,"voice":ID,"caption":CP,"parse_mode":"html"})
+     CKsend(redis,callback_query,type,ck,sID)
+     time.sleep(0.3)
+
+  if callback_query.message.reply_to_message.audio:
+    ID = callback_query.message.reply_to_message.audio.file_id
+    CP = callback_query.message.reply_to_message.caption
+    for sID in IDS:
+     ck = Bot("sendaudio",{"chat_id":sID,"audio":ID,"caption":CP,"parse_mode":"html"})
+     CKsend(redis,callback_query,type,ck,sID)
+     time.sleep(0.3)
+
+  if callback_query.message.reply_to_message.sticker:
+    ID = callback_query.message.reply_to_message.sticker.file_id
+    CP = callback_query.message.reply_to_message.caption
+    for sID in IDS:
+     ck = Bot("sendsticker",{"chat_id":sID,"sticker":ID,"caption":CP,"parse_mode":"html"})
+     CKsend(redis,callback_query,type,ck,sID)
+     time.sleep(0.3)
+     
+  if callback_query.message.reply_to_message.document:
+    ID = callback_query.message.reply_to_message.document.file_id
+    CP = callback_query.message.reply_to_message.caption
+    for sID in IDS:
+     ck = Bot("senddocument",{"chat_id":sID,"document":ID,"caption":CP,"parse_mode":"html"})
+     CKsend(redis,callback_query,type,ck,sID)
+     time.sleep(0.3)
+
+  if callback_query.message.reply_to_message.animation:
+    ID = callback_query.message.reply_to_message.animation.file_id
+    CP = callback_query.message.reply_to_message.caption
+    for sID in IDS:
+     ck = Bot("sendanimation",{"chat_id":sID,"animation":ID,"caption":CP,"parse_mode":"html"})
+     CKsend(redis,callback_query,type,ck,sID)
+     time.sleep(0.3)
+  
+
+  return redis.scard("{}Nbot:donesend".format(BOT_ID)),redis.scard("{}Nbot:dontsend".format(BOT_ID))
+
+
+
+def fwdto(redis,callback_query,type):
+  IDS = redis.smembers("{}Nbot:{}".format(BOT_ID,type))
+  if callback_query.message.reply_to_message.message_id:
+    for sID in IDS:
+      ck = Bot("forwardMessage",{"chat_id":sID,"from_chat_id":callback_query.message.chat.id,"message_id":callback_query.message.reply_to_message.message_id})
+      CKsend(redis,callback_query,type,ck,sID)
+      time.sleep(0.3)
+  return redis.scard("{}Nbot:donesend".format(BOT_ID)),redis.scard("{}Nbot:dontsend".format(BOT_ID))
+
+def sendM(T,msg,message):
+  if T == "NO":
+    chatID = message.chat.id
+    Len = 3000
+    msgs = [msg[y-Len:y] for y in range(Len, len(msg)+Len,Len)]
+    for tx in msgs:
+      Bot("sendMessage",{"chat_id":chatID,"text":tx,"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
+      time.sleep(0.3)
+
+def GetLink(chatID):
+  li = Bot("getchat",{"chat_id":chatID})["result"]
+  if "invite_link" in li:
+    return li["invite_link"]
+  else:
+    return False
+  
+
+def Name(name):
+  Len = 10
+  names = [name[y-Len:y] for y in range(Len, len(name)+Len,Len)]
+  return names[0]
